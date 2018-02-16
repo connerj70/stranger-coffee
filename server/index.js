ebb970fb0b1cded8594cd16af9a2b96c9d31d011
Conner Jensen <connerj70@gmail.com>
2018-02-16 10:15:18 -0700

    working

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ const express    = require('express'),
 
 const app = express();
 
+app.use( express.static( `${__dirname}/../build`));
+
+
 app.use(cors());
 app.use(bodyParser({limit: '50mb'}));
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import {Provider} from 'react-redux';
 import {HashRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(<Router><Provider store={store}><App /></Provider></Router>, document.getElementById('root'));
-registerServiceWorker();
