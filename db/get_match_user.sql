b9357970fba4a13e48456c8d7766d23cd1e0f70d
Conner Jensen <connerj70@gmail.com>
2018-02-19 11:28:26 -0700

    working

--- db/get_match_user.sql
+++ db/get_match_user.sql
@@ -1,2 +1,2 @@
 select * from users
-where lower(city) = $1 and id != $2;
+where lower(city) = $1 and id != $2 and id not in (select user2_id from matches) and id not in (select user1_id from matches)
