d7fb33336364dc42089553eb5507f68da07c3b99
Conner Jensen <connerj70@gmail.com>
2018-02-27 20:58:38 -0700

    working

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,20 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./Profile.css";
-import NavBar from '../../components/NavBar/NavBar';
-import Footer from '../../components/Footer/Footer';
-import {connect} from 'react-redux';
-import {getUserInfo, deleteMatch, getNotifications} from '../../ducks/reducer';
-import {Link} from 'react-router-dom';
-import Match from '../../components/Match/Match';
-import Modal from '../../components/Modal/Modal';
-import {ToastContainer, toast} from 'react-toastify';
-
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import { connect } from "react-redux";
+import {
+    getUserInfo,
+    deleteMatch,
+    getNotifications
+} from "../../ducks/reducer";
+import { Link } from "react-router-dom";
+import Match from "../../components/Match/Match";
+import Modal from "../../components/Modal/Modal";
+import { ToastContainer, toast } from "react-toastify";
 
 class Profile extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
-        this.state = {modal: false}
+        this.state = { modal: false };
         this.handleTrashClick = this.handleTrashClick.bind(this);
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
         this.handleCancelClick = this.handleCancelClick.bind(this);
@@ -26,7 +29,7 @@ class Profile extends Component {
     }
 
     handleTrashClick() {
-        this.setState((prevState) => {
+        this.setState(prevState => {
             return {
                 modal: !prevState.modal
             };
@@ -38,7 +41,7 @@ class Profile extends Component {
         this.setState({
             modal: false
         });
-        toast.success('Match deleted');
+        toast.success("Match deleted");
     }
 
     handleCancelClick() {
@@ -50,78 +53,111 @@ class Profile extends Component {
     render() {
         return (
             <div className="profile">
-            {
-            this.props.user.username ? 
-                <div className='profile_inner-container'>
-                    <NavBar background={true} />
-                    <div className="profile_card-and-button-container">
-                        <div className="profile_card-header">
-                            <h1>Profile</h1>
-                        </div>
-                        <div className="profile_card">
-                            <div className="profile_card-image-container">
-                                <img alt="user profile" src={this.props.user.profile_pic} />
+                {this.props.user.username ? (
+                    <div className="profile_inner-container">
+                        <NavBar background={true} />
+                        <div className="profile_card-and-button-container">
+                            <div className="profile_card-header">
+                                <h1>Profile</h1>
                             </div>
-                            <div className="profile_card-user-info-container">
-                                <div className="profile_card-user-sub-info-container">
-                                    <div className="profile_card-bold">Username: </div>
-                                    <div>{this.props.user.username}</div>
+                            <div className="profile_card">
+                                <div className="profile_card-image-container">
+                                    <img
+                                        alt="user profile"
+                                        src={this.props.user.profile_pic}
+                                    />
                                 </div>
-                                <div className="profile_card-user-sub-info-container">
-                                    <div className="profile_card-bold">Nickname: </div>
-                                    <div>{this.props.user.nickname}</div>
-                                </div>
-                                <div className="profile_card-user-sub-info-container">
-                                    <div className="profile_card-bold">City: </div>
-                                    <div>{this.props.user.city}</div>
-                                </div>
-                                <div className="profile_card-user-sub-info-container">
-                                    <div className="profile_card-bold">State: </div>
-                                    <div>{this.props.user.state}</div>
-                                </div>
-                                <div className="profile_card-user-sub-info-container">
-                                    <div className="profile_card-bold">Hobby: </div>
-                                    <div>{this.props.user.hobby}</div>
+                                <div className="profile_card-user-info-container">
+                                    <div className="profile_card-user-sub-info-container">
+                                        <div className="profile_card-bold">
+                                            Username:{" "}
+                                        </div>
+                                        <div>{this.props.user.username}</div>
+                                    </div>
+                                    <div className="profile_card-user-sub-info-container">
+                                        <div className="profile_card-bold">
+                                            Nickname:{" "}
+                                        </div>
+                                        <div>{this.props.user.nickname}</div>
+                                    </div>
+                                    <div className="profile_card-user-sub-info-container">
+                                        <div className="profile_card-bold">
+                                            City:{" "}
+                                        </div>
+                                        <div>{this.props.user.city}</div>
+                                    </div>
+                                    <div className="profile_card-user-sub-info-container">
+                                        <div className="profile_card-bold">
+                                            State:{" "}
+                                        </div>
+                                        <div>{this.props.user.state}</div>
+                                    </div>
+                                    <div className="profile_card-user-sub-info-container">
+                                        <div className="profile_card-bold">
+                                            Hobby:{" "}
+                                        </div>
+                                        <div>{this.props.user.hobby}</div>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="profile_button-container">
+                                <Link to="/editprofile">
+                                    <button className="profile_edit-button">
+                                        EDIT
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="profile_match-main-header">
+                                <h1>Match</h1>
+                            </div>
+                            <Match
+                                handleTrashClick={this.handleTrashClick}
+                                title="Current Match"
+                                id={this.props.user.id}
+                                city={this.props.user.city}
+                            />
+                            <div className="profile_review-button-container">
+                                <Link to="/createreview">
+                                    <button id="profile_review-match-button">
+                                        Review Previous Match
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
-                        <div className="profile_button-container">
-                            <Link to="/editprofile"><button className="profile_edit-button">EDIT</button></Link>
-                        </div>
-                    <div className="profile_match-main-header">
-                        <h1>Match</h1>
+
+                        <Footer />
+                        {this.state.modal ? (
+                            <Modal
+                                handleDeleteClick={this.handleDeleteClick}
+                                handleCancelClick={this.handleCancelClick}
+                                title="Delete"
+                                text="Are you sure you want to delete this match?"
+                                button1Text="Delete"
+                                button2Text="Cancel"
+                            />
+                        ) : null}
                     </div>
-                    <Match handleTrashClick={this.handleTrashClick} title="Current Match" id={this.props.user.id} city={this.props.user.city}/>
-                    <div className="profile_review-button-container">
-                        <Link to="/createreview"><button id="profile_review-match-button">Review Previous Match</button></Link>
+                ) : (
+                    <div>
+                        Unauthorized
+                        <Link to="/">Return to home page.</Link>
                     </div>
-                    </div>
-                   
-                    <Footer />
-                    {
-                        this.state.modal ?
-                    <Modal  handleDeleteClick={this.handleDeleteClick} handleCancelClick={this.handleCancelClick} title="Delete" text="Are you sure you want to delete this match?" button1Text="Delete" button2Text="Cancel"/>
-                    :
-                    null
-                    }
-                </div>
-                :
-                <div>
-                    Unauthorized
-                    <Link to="/">Return to home page.</Link>
-                </div>
-            }
-            <ToastContainer autoClose={1000} />
+                )}
+                <ToastContainer autoClose={1500} />
             </div>
-        )
+        );
     }
 }
 
-function mapStateToProps({user, currentMatch}) {
+function mapStateToProps({ user, currentMatch }) {
     return {
         user,
         currentMatch
-    }
+    };
 }
 
-export default connect(mapStateToProps, {getUserInfo, deleteMatch, getNotifications})(Profile);
+export default connect(mapStateToProps, {
+    getUserInfo,
+    deleteMatch,
+    getNotifications
+})(Profile);
