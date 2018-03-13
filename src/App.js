0cde3a939d428b16652c1c1802826146836d97e5
Conner Jensen <connerj70@gmail.com>
2018-03-13 14:53:46 -0600

    w

--- src/App.js
+++ src/App.js
@@ -9,13 +9,51 @@ import About from "./views/About/About";
 import ReviewDetails from "./components/ReviewDetails/ReviewDetails";
 import { Switch, Route } from "react-router-dom";
 
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-102758287-2", {
+    debug: true,
+    gaOptions: { cookieDomain: "none" }
+});
+
+function withTracker(WrappedComponent, options = {}) {
+    const trackPage = page => {
+        ReactGA.set({
+            page,
+            ...options
+        });
+        ReactGA.pageview(page);
+    };
+
+    const HOC = class extends Component {
+        componentDidMount() {
+            const page = this.props.location.pathname;
+            trackPage(page);
+        }
+
+        componentWillReceiveProps(nextProps) {
+            const currentPage = this.props.location.pathname;
+            const nextPage = nextProps.location.pathname;
+
+            if (currentPage !== nextPage) {
+                trackPage(nextPage);
+            }
+        }
+
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+
+    return HOC;
+}
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/profile" component={Profile} />
+                    <Route exact path="/" component={withTracker(Home)} />
+                    <Route path="/profile" component={withTracker(Profile)} />
                     <Route path="/editprofile" component={EditProfile} />
                     <Route path="/createreview" component={CreateReview} />
                     <Route path="/about" component={About} />
